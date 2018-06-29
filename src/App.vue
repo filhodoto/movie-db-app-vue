af2ef417b3025586b02f72719e550667b2e2082b
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-06-29 12:16:37 +0200

    Small changes, add header
    - Add some css
    - Create Header component
    - change index-html container id

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :title="headerTitle" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      headerTitle: 'movie database',
+    }
+  },
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
+  body {
+    margin: 0;
+  }
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <img src="./../assets/logo.png"/><h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    props: {
+      title: String
+    },
+  }
+</script>
+
+<style scoped>
+    header {
+        background-color: #111;
+        padding: 20px;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 50px;
+        margin-right: 20px;
+    }
+
+</style>
